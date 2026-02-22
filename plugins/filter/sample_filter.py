94f43945ef2f5bfb929ac22d27c7640a4bd8c02a
Carlos Meza <carlos@digitalr00ts.com>
2026-02-22 08:06:03 -0800

    test(molecule): fix

---
+++ plugins/filter/sample_filter.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2026, Carlos Meza <carlos@digitalr00ts.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+"""Sample filter plugin for testing collection infrastructure."""
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+
+def sample_filter(value):
+    """
+    Simple filter that prepends 'Hello, ' to the input value.
+
+    Args:
+        value (str): The input string to process
+
+    Returns:
+        str: The input string with 'Hello, ' prepended
+
+    Examples:
+        >>> sample_filter('world')
+        'Hello, world'
+        >>> sample_filter('ansible-creator')
+        'Hello, ansible-creator'
+    """
+    return f"Hello, {value}"
+
+
+class FilterModule:
+    """Ansible filter plugin class."""
+
+    def filters(self):
+        """
+        Return filter plugin mappings.
+
+        Returns:
+            dict: Dictionary mapping filter names to filter functions
+        """
+        return {
+            "sample_filter": sample_filter,
+        }
